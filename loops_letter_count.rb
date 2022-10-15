882489d27a2e62d7736f564d5f27f60cf1e98593
gsista-booth <gsista@chicagobooth.edu>
2022-10-15 17:37:16 +0000

    loops

--- loops_letter_count.rb
+++ loops_letter_count.rb
@@ -1,9 +1,9 @@
 # Write a program that:
-# 
+#
 # Asks the user to input a word and counts from 1 to however long the word is.
-# 
+#
 # Example (`apple` is the input):
-# 
+#
 # "Enter a word:"
 # apple
 # 1
@@ -14,3 +14,16 @@
 # "apple is 5 letters long!"
 
 p "Enter a word:"
+
+word = gets.chomp
+
+word_length = word.length
+
+number = 0
+
+while number < word_length
+  number = number + 1
+  p number
+end
+
+p word + " is " + word_length.to_s + " letters long!"
